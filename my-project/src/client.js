9739a88e89a850739dc64f1ee4ce683c21a4c08a
Dashrath Patel <108078023+ULTRAPAIN@users.noreply.github.com>
2024-07-21 17:54:46 +0530

    Change the API Version

--- my-project/src/client.js
+++ my-project/src/client.js
@@ -4,7 +4,7 @@ import imageUrlBuilder from '@sanity/image-url';
 export const client = createClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: 'production',
-  apiVersion: '2023-10-04',
+  apiVersion: '2022-03-07',
   useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
 });
